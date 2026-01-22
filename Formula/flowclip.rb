dce018b3b4843ca588d45252fb3b6bad033dae81
gityeop <freiheit517@icloud.com>
2026-01-22 22:33:25 +0900

    Release: Bump version to 1.0.4 (Improved Full Bleed Icon)

--- Formula/flowclip.rb
+++ Formula/flowclip.rb
@@ -1,8 +1,8 @@
 cask "flowclip" do
-  version "1.0.3"
-  sha256 "a64f8c0866e4440fb170fb56fc3a760b06a379f87bd20e1d109ff51d42d9f4d2"
+  version "1.0.4"
+  sha256 "faadb94eb68b93fe5f0a08ee81d632b118c1b42a72f4397b1b6f4f7e29324602"
 
-  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_1.0.3.zip"
+  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_1.0.4.zip"
   name "FlowClip"
   desc "Clipboard manager with Queue support (Fork of Maccy)"
   homepage "https://github.com/gityeop/FlowClip"
