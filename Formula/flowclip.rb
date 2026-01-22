e733525e4072304c8c3f427884b3cc5a56d7101c
gityeop <freiheit517@icloud.com>
2026-01-22 19:26:35 +0900

    Formula: Update SHA256 for release 2.6.1

--- Formula/flowclip.rb
+++ Formula/flowclip.rb
@@ -1,8 +1,8 @@
 cask "flowclip" do
   version "2.6.1"
-  sha256 "replace_with_actual_sha256_of_zip"
+  sha256 "301f9e1bd4515ac98f7660e3b08d9ca655a0f7c6178657c5ee13adbdd471c47b"
 
-  url "https://github.com/gityeop/FlowClip/releases/download/#{version}/FlowClip.zip"
+  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_2.6.1.zip"
   name "FlowClip"
   desc "Clipboard manager with Queue support (Fork of Maccy)"
   homepage "https://github.com/gityeop/FlowClip"
