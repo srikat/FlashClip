221edeb3e83f9a27f917d8ab7ed6b47a64f573ed
gityeop <freiheit517@icloud.com>
2026-01-24 19:40:19 +0900

    refactor: cleanup clipboard paste simulation and update test configs

--- Maccy/Clipboard.swift
+++ Maccy/Clipboard.swift
@@ -133,8 +133,6 @@ class Clipboard {
     let keyVUp = CGEvent(keyboardEventSource: source, virtualKey: vCode, keyDown: false)
     keyVDown?.flags = cmdFlag
     keyVUp?.flags = cmdFlag
-    keyVDown?.setIntegerValueField(.eventSourceUserData, value: 55555)
-    keyVUp?.setIntegerValueField(.eventSourceUserData, value: 55555)
     keyVDown?.post(tap: .cgSessionEventTap)
     keyVUp?.post(tap: .cgSessionEventTap)
   }
